9347ce2ba8e7f616f2d2a13a9c7f93588c7aa021
Keith Tunstead <tunstek@tcd.ie>
2018-12-04 20:16:26 +0000

    added insertion sort

--- sort.erl
+++ sort.erl
@@ -1,5 +1,5 @@
 -module(sort).
--export([bubble/1]).
+-export([bubble/1, insertion/1]).
 -compile([debug_info]).
 
 bubble([]) ->
@@ -19,6 +19,28 @@ bubble(Acc, [H,J|T], _) when H=<J ->
     bubble(lists:append(Acc,[H]), lists:append([J], T), false);
 bubble(Acc, [H,J|T], _) when H>J ->
     bubble(lists:append(Acc,[J]), lists:append([H], T), true).
+
+
+
+insertion(L) ->
+    insertionAcc([], L).
+
+insertionAcc(Acc, []) ->
+    Acc;
+insertionAcc(Acc, [H|[]]) ->
+    insert(Acc, H);
+insertionAcc(Acc, [H|T]) ->
+    insertionAcc(insert(Acc, H), T).
+
+insert([], E) ->
+    [E];
+insert([H|T], E) when E >= H ->
+    lists:append([H], insert(T, E));
+insert([H|T], E) when E < H ->
+    lists:append(lists:append([E], [H]), T).
+
+
+
 
 
 
